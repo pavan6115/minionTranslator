ebed5b5233457f34e70f738a040bffad343dd512
kpavann <kulkarnipavan6115@gmail.com>
2021-08-13 19:58:06 +0530

    feat:function updated for showing output when clicked

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log('connected!')
 const translateBtn = document.querySelector('#translateBtn')
 const inpText = document.querySelector('.inpText')
+const outputSection = document.querySelector('.output')
 
 function clickHandler() {
-    console.log('Button Clicked')
-    console.log('Input from Textarea -', inpText.value)
+    // tells the button what to do when clicked 
+    // main task - translate and show in output
+    outputSection.innerHTML = "Hey there, coming from JS world!" + inpText.value
 }
 
 translateBtn.addEventListener('click', clickHandler)
